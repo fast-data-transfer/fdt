a829f9839ea32aa6f2418e6dbd99d65eb4688d4a
Raimondas Sirvinskas <raimis.sirvis@gmail.com>
2017-08-07 12:21:57 +0300

    Fix cast exception while saving last update check time

--- src/lia/util/net/common/Utils.java
+++ src/lia/util/net/common/Utils.java
@@ -1105,7 +1105,7 @@ public final class Utils {
 
                     }
 
-                    updateProperties.put("LastCheck", now);
+                    updateProperties.put("LastCheck", String.valueOf(now));
 
                     try {
                         fos = new FileOutputStream(confFile);
