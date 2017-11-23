39a3b4d0fe8d407f70d057c57d0080c6dee55047
Raimondas Širvinskas <raimis.sirvis@gmail.com>
2017-11-23 23:07:08 +0200

    Update release date

--- src/lia/util/net/common/Config.java
+++ src/lia/util/net/common/Config.java
@@ -46,7 +46,6 @@ public class Config {
     public static final String REGEX_REMAP_DELIMITER = "(\\s)+/(\\s)+";
     // all of this are set by the ant script
     public static final String FDT_MAJOR_VERSION = "0";
-
     public static final String FDT_MINOR_VERSION = "26";
     public static final String FDT_MAINTENANCE_VERSION = "1";
     public static final String FDT_FULL_VERSION = FDT_MAJOR_VERSION + "." + FDT_MINOR_VERSION + "."
