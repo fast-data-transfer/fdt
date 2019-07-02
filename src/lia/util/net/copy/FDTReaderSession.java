3a796501666ae6beffb9e94b5a9a3bfb2d169fe5
grandmasterlinux <grandmasterlinux@gmail.com>
2019-07-02 05:51:21 -0500

    apply same bugfix from FDTWriterSession for : to /

--- src/lia/util/net/copy/FDTReaderSession.java
+++ src/lia/util/net/copy/FDTReaderSession.java
@@ -60,7 +60,7 @@ public class FDTReaderSession extends FDTSession implements FileBlockProducer {
      */
     public FDTReaderSession(int transferPort) throws Exception {
         super(FDTSession.CLIENT, transferPort);
-        Utils.initLogger(config.getLogLevel(), new File(System.getProperty("java.io.tmpdir") + File.pathSeparatorChar + sessionID + ".log"), new Properties());
+        Utils.initLogger(config.getLogLevel(), new File(System.getProperty("java.io.tmpdir") + File.separatorChar + sessionID + ".log"), new Properties());
         final int rMul = Integer.getInteger("fdt.rQueueM", 2);
         final int avProcProp = Integer.getInteger("fdt.avProc", 1);
         final int avProcMax = Math.max(avProcProp, Utils.availableProcessors());
